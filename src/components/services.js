34fe9376386ddb427cc882825d6e310b0f5bcc2d
github-project-files <github.project.files@gmail.com>
2022-02-04 09:42:30 +0530

    header and contact form update

--- src/components/services.js
+++ src/components/services.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import BreadCrumbBanner from './banner';
+import Footer from "./footer";
 
 function services() {
-  return <div className='container'>
+  return <div><BreadCrumbBanner/>
+  <div className='container'>
   <h2>What Is Lorem Ipsum?</h2>
   <p>Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+<Footer/>
 </div>;
 }
 
